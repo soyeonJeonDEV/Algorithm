3a29bbce4f0da9f8b349c90363af38318e1d2056
writinghaetae <79301243+writinghaetae@users.noreply.github.com>
2021-02-27 11:13:42 +0900

    codeup_2001
    코드업 2001 그리디 알고리즘 정답 소스코드

---
+++ codeup_greedy_2001.py
@@ -0,0 +1,13 @@
+pasta=[]
+for i in range(3):
+	pasta.append(float(input()))
+juice=[]
+for i in range(2):
+	juice.append(float(input()))
+
+p1 = min(pasta)
+j1 = min(juice)
+
+price = (p1+j1)*1.1
+
+print("%.1f"%price)
