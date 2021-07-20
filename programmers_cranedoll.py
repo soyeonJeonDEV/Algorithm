4e23e9901ea6a97ece4afb4d7b0e484b3965c1ba
repl.it user <writinghaetae@gmail.com>
2021-07-20 01:26:52 +0000

    commit

---
+++ programmers_cranedoll.py
@@ -0,0 +1,22 @@
+#크레인인형뽑기
+#https://programmers.co.kr/learn/courses/30/lessons/64061
+
+def solution(board, moves):
+    basket = []
+    answer = 0
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] == 0:
+                 pass
+            else:
+                basket.append(board[j][i-1])
+                board[j][i-1] = 0
+                break;
+                
+        if len(basket) >= 2 and basket[len(basket)-1] == basket[len(basket)-2]:
+            basket.pop(-1)
+            basket.pop(-1)
+            answer += 1
+    return answer * 2
+
