8d661853d8b79ee3a122407aa4b34d48b6cc441f
repl.it user <writinghaetae@gmail.com>
2021-08-02 06:03:36 +0000

    commit

---
+++ programmers_new_id.py
@@ -0,0 +1,42 @@
+#신규아이디 추천
+#https://programmers.co.kr/learn/courses/30/lessons/72410
+def solution(new_id):
+    #1단계
+    new_id = new_id.lower()
+    #2단계
+    answer = ''
+    for word in new_id:
+        if word.isalnum() or word in '-_.':
+            answer += word
+    #3단계
+    while '..' in answer:
+        answer = answer.replace('..','.')
+    #4단계
+    answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer
+    answer = answer[:-1] if answer[-1] == '.' else answer
+    #5단계
+    answer = 'a' if answer == '' else answer
+    #6단계
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    #7단계
+    if len(answer) <= 3:
+        answer = answer + answer[-1] * (3-len(answer))
+    return answer
+
+    '''
+    import re
+
+def solution(new_id):
+    st = new_id
+    st = st.lower()
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+    st = re.sub('\.+', '.', st)
+    st = re.sub('^[.]|[.]$', '', st)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+    return st
+    '''
---
+++ programmers_weekly1.py
@@ -0,0 +1,15 @@
+#위클리챌린지1주차
+#부족한금액 계산하기
+#https://programmers.co.kr/learn/courses/30/lessons/82612
+def solution(price, money, count):
+    answer = 0
+    total = 0
+    
+    for i in range(count):
+        total += price*(i+1)
+        print(total)
+
+    if total - money <= 0:
+        return 0
+    else:
+        return total - money
