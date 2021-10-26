c10dddf4b4a22c0af384176d13a21724dd31fae5
soyeonJeonDEV <79301243+soyeonJeonDEV@users.noreply.github.com>
2021-10-26 11:25:01 +0900

    Update programmers_나머지가 1이 되는 수 찾기(간단한 코드 추가)

--- programmers/Level1/programmers_나머지가1이되는수찾기.py
+++ programmers/Level1/programmers_나머지가1이되는수찾기.py
@@ -1,10 +1,15 @@
 #https://programmers.co.kr/learn/courses/30/lessons/87389
 #나머지가 1이 되는 수 찾기
-
+'''
 def solution(n):
     for i in range(1,n):
         if n%i == 1:
             answer = i
             break
             
+    return answer
+'''
+# 더 좋은 코드
+def solution(n):
+    answer = min([x for x in range(1,n) if n%x == 1])
     return answer
