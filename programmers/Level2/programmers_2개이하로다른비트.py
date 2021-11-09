47e141b02b3f9f30e211851d32453d9715691275
soyeonJeonDEV <writinghaetae@gmail.com>
2021-11-09 10:04:36 +0900

    Update programmers - 2개 이하로 다른 비트(시간초과2)

--- programmers/Level2/programmers_2개이하로다른비트.py
+++ programmers/Level2/programmers_2개이하로다른비트.py
@@ -1,20 +1,13 @@
 def solution(numbers):
     answer = []
     for i in numbers:
-        num1_bin = str(bin(i))[2:]
+        num1 = i
         num2 = i
         while True:
             num2 += 1
-            num2_bin = str(bin(num2))[2:]
-            bit = 0
-            z = len(num1_bin)+2
-            num1_bin = num1_bin.rjust(z,'0')
-            num2_bin = num2_bin.rjust(z,'0')            
-            for j in range(len(num1_bin)):
-                if num1_bin[j] != num2_bin[j]:
-                    bit += 1
-            if 0 < bit <= 2:
+            a = bin(num1 ^ num2)
+            bit = a.count('1')
+            if 0<bit<=2:
                 answer.append(num2)
                 break
-                
     return answer
