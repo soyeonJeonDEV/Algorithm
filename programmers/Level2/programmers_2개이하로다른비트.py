62d810ebf47517fdd57e34965787b7ff411b54ef
soyeonJeonDEV <writinghaetae@gmail.com>
2021-11-09 10:45:00 +0900

    Update programmers - 2개 이하로 다른 비트

--- programmers/Level2/programmers_2개이하로다른비트.py
+++ programmers/Level2/programmers_2개이하로다른비트.py
@@ -1,13 +1,12 @@
 def solution(numbers):
     answer = []
     for i in numbers:
-        num1 = i
-        num2 = i
-        while True:
-            num2 += 1
-            a = bin(num1 ^ num2)
-            bit = a.count('1')
-            if 0<bit<=2:
-                answer.append(num2)
-                break
+        bin_num = list('0' + bin(i)[2:])
+        #rfind : 오른쪽부터 처음 등장하는 위치를 찾음
+        #짝수의 경우 2진수 마지막 숫자는 '0'
+        idx = ''.join(bin_num).rfind('0')
+        bin_num[idx] = '1'
+        if i % 2 == 1:
+            bin_num[idx+1] = '0'
+        answer.append(int(''.join(bin_num),2))
     return answer
