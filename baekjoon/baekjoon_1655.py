4d11118fc21a1b43ee6c6708cfda73b8ff95dbe5
soyeonJeonDEV <writinghaetae@gmail.com>
2021-11-23 10:12:05 +0900

    Update baekjoon - 가운데를 말해요

--- baekjoon/baekjoon_1655.py
+++ baekjoon/baekjoon_1655.py
@@ -1,20 +1,28 @@
 #https://www.acmicpc.net/problem/1655
 #가운데를 말해요
-import sys
+#heapq 모듈을 사용
 
-input = sys.stdin.readline()
+import sys
+import heapq
+input = sys.stdin.readline
 
 n = int(input())
-num = []
-for i in range(n):
-    num.append(int(input()))
-    if i == 0: 
-        print(num[i])
-        continue
-    else:
-        num = sorted(num)
-        if len(num) % 2 == 0:
-            print(num[len(num)//2-1])
-        else:
-            print(num[len(num)//2])
+left, right = [],[]
 
+for _ in range(n):
+    num = int(input())
+    if len(left) == len(right):
+        #최댓값 정렬
+        heapq.heappush(left,(-num,num))
+    else:
+        #최소값 정렬
+        heapq.heappush(right,(num,num))
+
+    if right and left[0][1] > right[0][1]:
+        left_value = heapq.heappop(left)[1]
+        right_value = heapq.heappop(right)[1]
+        print(left_value)
+        print(right_value)
+        heapq.heappush(right,(left_value,left_value))
+        heapq.heappush(left,(-right_value,right_value))
+    print(left[0][1])
