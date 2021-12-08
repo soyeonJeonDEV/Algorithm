e533fd8338a270820a55bbae49272650f5c1eee7
JeonSoyeon <79301243+soyeonJeonDEV@users.noreply.github.com>
2021-12-08 22:53:17 +0900

    Update programmers_match.py

--- programmers/Level1/programmers_match.py
+++ programmers/Level1/programmers_match.py
@@ -1,27 +1,17 @@
 #짝지어 제거하기
 #https://programmers.co.kr/learn/courses/30/lessons/12973
-#내가 푼 풀이 : 런타임 에러로 인한 오류
-'''
-def solution(s):
-    basket = []
-    for i in s:
-        basket.append(i)
-        for j in range(len(basket)):
-            if len(basket) >= 2 and basket[j] == basket[j-1]:
-                basket.pop(-1)
-                basket.pop(-1)
-            
 
-    return 1 if basket == []  else 0
-'''
 def solution(s):
-    stack = []
-    for i in range(len(s)):
-        if not stack:
-            stack.append(s[i])
-        else :
-            if s[i] == stack[-1]:
-                stack.pop()
+    answer = []
+    for i in s:
+        if len(answer) == 0:
+            answer.append(i)
+        else:
+            #answer에 마지막으로 들어온 값과 i를 비교한 후 같으면 answer의 마지막 값을 빼고
+            if answer[-1] == i:
+                answer.pop()
+            #그렇지 않으면 answer에 i값을 추가한다.
             else:
-                stack.append(s[i])
-    return 0 if stack else 1
+                answer.append(i)
+    #answer가 존재하면 짝지어 제거하기 실패이므로 0 아니면 1을 리턴한다.            
+    return 0 if answer else 1
