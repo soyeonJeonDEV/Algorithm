57354a18a0b746e70b2287b26599fbdabb73a46f
soyeonJeonDEV <writinghaetae@gmail.com>
2021-12-10 16:26:07 +0900

    주석 추가

--- programmers/Level2/programmers_targetnumber.py
+++ programmers/Level2/programmers_targetnumber.py
@@ -1,28 +1,20 @@
 #타겟넘버
 #https://programmers.co.kr/learn/courses/30/lessons/43165
-#공부 후 다시 작성
+#답 보고 작성
+
 def solution(numbers, target):
-  tree = [0]
-  for num in numbers:
-    sub_tree = []
-    for tree_num in  tree:
-      sub_tree.append(tree_num + num)
-      sub_tree.append(tree_num - num)
-    tree = sub_tree
-  return tree.count(target)
-  '''
-  def solution(numbers, target):
-    if not numbers and target == 0:
-      return 1
-    elif not numbers:
-      return 0
-    else :
-      return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
-  '''
-  '''
-  from itertools import product
-  def solution(numbers, target):
-    l = [(x,-x) for x in numbers]
-    s = list(map(sum, product(*l)))
-    return s.count(target)
-  '''
+    tree = [0]
+    for num in numbers:
+        #sub_tree를 매번 초기화시킨다.
+        sub_tree = []
+        #1. 0 + 1, 0 - 1 의 값을 sub_tree에 집어넣고 tree에 대입한다.
+        #2. tree = [1,-1]
+        #3. 1 + 1, 1 - 1, -1 + 1, -1 - 1의 값을 sut_tree에 집어넣는다.
+        #4. tree = [2,0,0,-2]
+        #5. 반복
+        for tree_num in tree:
+            sub_tree.append(tree_num + num)
+            sub_tree.append(tree_num - num)
+        tree = sub_tree
+    #tree의 값에 numbers로 더하고 빼는 모든 경우의 수가 들어갔다면 target값과 같은 개수를 반환한다. 
+    return tree.count(target)
