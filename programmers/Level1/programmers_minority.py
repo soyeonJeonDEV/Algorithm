3ba52d1a7c619868973975e017c8ad91d5b6563c
soyeonJeonDEV <writinghaetae@gmail.com>
2021-12-10 16:26:17 +0900

    Update programmers_minority.py

--- programmers/Level1/programmers_minority.py
+++ programmers/Level1/programmers_minority.py
@@ -2,22 +2,19 @@
 #소수 만들기
 
 def solution(nums):
-    answer = 0
-    nums.sort()
     temp = []
-
+    cnt = 0
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             for k in range(j+1, len(nums)):
-                temp.append(nums[i]+nums[j]+nums[k])
+                temp.append(nums[i] + nums[j] + nums[k])
     
-    flag = True
     for i in temp:
-        for j in range(2,i):
-            if i%j == 0:
-                flag = False
-                break
-        if flag:
-            answer += 1
         flag = True
-    return answer
+        for j in range(2,i):
+            if i % j == 0:
+                flag = False      
+        if flag:
+            cnt += 1
+        
+    return cnt
