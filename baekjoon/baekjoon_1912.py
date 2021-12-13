96306107b7896c3e496761bd95d18cb409477dbe
soyeonJeonDEV <writinghaetae@gmail.com>
2021-12-13 15:05:55 +0900

    주석 추가

--- baekjoon/baekjoon_1912.py
+++ baekjoon/baekjoon_1912.py
@@ -5,6 +5,7 @@ n = int(input())
 arr = list(map(int,input().split()))
 dp = [arr[0]]
 for i in range(len(arr)-1):
+  #dp에 이전값과 현재값의 합과 현재값 중에 큰값을 dp에 넣는다.
   dp.append(max(dp[i] + arr[i+1], arr[i+1]))
-  
+#dp값중 최대값을 출력한다.
 print(max(dp))
