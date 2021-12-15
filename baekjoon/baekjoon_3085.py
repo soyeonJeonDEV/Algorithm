3882f0ef87bf39b3548de7929d23cf96568498a9
soyeonJeonDEV <writinghaetae@gmail.com>
2021-12-15 11:37:12 +0900

    Update baekjoon_3085.py

--- baekjoon/baekjoon_3085.py
+++ baekjoon/baekjoon_3085.py
@@ -1,49 +1,53 @@
 #https://www.acmicpc.net/problem/3085
 #사탕게임
 
-import copy
-import sys
+def checkRow(a):
+    maxNum = 1
+    for i in range(n):
+        cnt = 1
+        for j in range(1,n):
+            if a[i][j] == a[i][j-1]:
+                cnt += 1
+                maxNum = max(maxNum, cnt)
+            else:
+                cnt = 1
+    return maxNum
 
-input = sys.stdin.readline
+def checkCol(a):
+    maxNum = 1
+    for j in range(n):
+        cnt = 1
+        for i in range(1,n):
+            if a[i][j] == a[i-1][j]:
+                cnt += 1
+                maxNum = max(maxNum, cnt)
+            else:
+                cnt = 1
+    return maxNum
 
 n = int(input())
 board = [list(map(str,input())) for _ in range(n)]
 
-def checkRow(arr, line):
-    cntRow = 1
-    for i in range(len(arr)-1):
-        if arr[line][i] == arr[line][i+1]:
-            cntRow += 1
-    return cntRow
+arr = board
+result = 0
 
-def checkCol(arr, line):
-    cntCol = 1
-    for i in range(len(arr)-1):
-        if arr[i][line] == arr[i+1][line]:
-            cntCol += 1
-    return cntCol
+for i in range(n):
+    for j in range(n-1):
+        if arr[i][j] != arr[i][j+1]:
+            arr[i][j], arr[i][j+1] = arr[i][j+1],arr[i][j]
+            a = checkRow(arr)
+            result = a if a > result else result
+            a = checkCol(arr)
+            result = a if a > result else result
+            arr[i][j], arr[i][j+1] = arr[i][j+1],arr[i][j]
 
-answer = 0
-a = 0
-b = 0
-
-for i in range(len(board)):
-    answer = max(checkRow(board,i), checkCol(board,i), answer)
-
-#행과 열의 사탕을 swap한 값
-for i in range(len(board)):
-    for j in range(len(board)-1):
-        row = copy.deepcopy(board)
-        column = copy.deepcopy(board)
-        if board[i][j] != board[i][j+1]:
-            row[i][j], row[i][j+1] = board[i][j+1], board[i][j]
-            a = checkRow(row,i)
-            b = checkCol(row,j)
-            answer = max(a,b,answer)
-        if board[j][i] != board[j+1][i]:
-            column[j][i], column[j+1][i] = board[j+1][i], board[j][i]
-            a = checkRow(column,i)
-            b = checkCol(column,j)
-            answer = max(a,b,answer)
-
-print(answer)
+for i in range(n-1):
+    for j in range(n):
+        if arr[i][j] != arr[i+1][j]:
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            a = checkRow(arr)
+            result = a if a > result else result
+            a = checkCol(arr)
+            result = a if a > result else result
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+print(result)
