ab444a948fe4c1e80364277330ee242363624c29
soyeonJeonDEV <writinghaetae@gmail.com>
2021-12-17 09:13:01 +0900

    Create baekjoon_4796.py

---
+++ baekjoon/baekjoon_4796.py
@@ -0,0 +1,21 @@
+#캠핑
+
+i  = 1
+while True:
+    l, p, v = map(int,input().split())
+    rest = p - l
+    day = 0
+
+    if l == 0 and p == 0 and v == 0:
+        break
+    
+    while v > 0:
+        v -= l
+        day += l
+        if v > 0:
+            v -= rest
+        else:
+            day += v
+    print("Case {}: {}".format(i, day))
+    i += 1
+
