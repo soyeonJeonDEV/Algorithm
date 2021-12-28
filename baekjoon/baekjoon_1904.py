4f4d1aa3e8bd9cecfbd0fc13b42edbb6b52374e8
writinghaetae <79301243+writinghaetae@users.noreply.github.com>
2021-12-28 23:11:27 +0900

    Create baekjoon_1904.py

---
+++ baekjoon/baekjoon_1904.py
@@ -0,0 +1,14 @@
+#https://www.acmicpc.net/problem/1904
+#01타일
+
+n = int(input())
+
+dp = [0] * 1000000
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3,n+1):
+    dp[i] = (dp[i-1] + dp[i-2])%15746
+
+print(dp[-1])
