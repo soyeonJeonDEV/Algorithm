220d341dc45c744af9b4aed4b5f9a97207f64338
writinghaetae <79301243+writinghaetae@users.noreply.github.com>
2022-01-04 21:08:05 +0900

    Create baekjoon_11726.py

---
+++ baekjoon/silver/baekjoon_11726.py
@@ -0,0 +1,11 @@
+#https://www.acmicpc.net/problem/11726
+#2*n타일링
+n = int(input())
+dp = [0 for _ in range(1001)]
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3,1001):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n]%10007)
